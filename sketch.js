d3476595516eafd8067043359416815c095c9678
eltroxa <106834124+eltroxa@users.noreply.github.com>
2022-06-03 14:58:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box;
+function setup() {
+  createCanvas(600,600);
+  box = createSprite (300,300,30,30);
+  box.shapeColor ="red"
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+}
+
+
+
+//não apaga po
